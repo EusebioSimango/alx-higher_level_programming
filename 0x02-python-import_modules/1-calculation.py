6545f23fb74d4a73a0052141d94d86b0be253e30
Eusebio Simango <56705157+EusebioSimango@users.noreply.github.com>
2023-03-10 16:31:41 +0200

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import calculator_1
-    a = 10
-    b = 5
+    a: int = 10
+    b: int = 5
     print(f"{a} + {b} = {calculator_1.add(a, b)}")
     print(f"{a} - {b} = {calculator_1.sub(a, b)}")
     print(f"{a} * {b} = {calculator_1.mul(a, b)}")
